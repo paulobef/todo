c6d1cd961dbc29c6133a757ca2c0580c1660eadc
Paul Beffa <paulbeffa@outlook.fr>
2019-03-25 19:53:06 +0100

    new templating logic

--- js/Item.js
+++ js/Item.js
@@ -1,10 +1,12 @@
 const today = new Date();
 
 class Item {
-    constructor(text, dueDate) {
+    constructor(text, dueDate, id) {
+        this.id = id
         this.text = text;
         this.dueDate = dueDate;
         this.isDue = false;
+        this.isDone = false;
     }
     isDateDue() {
 
@@ -12,24 +14,34 @@ class Item {
             this.isDue = true;
         }
     }
-    template() {
-        let opening = `<div class="container">
-        <div class="row">`;
-        let closing = `</div>
-        </div>`;
-        let item = `<div class="col-sm-8">
-               ${this.text}
-             </div>
-             <div class="col-sm-3">
-               ${this.dueDate}
-             </div>
-             <div class="col-sm-1">`;
-        let dueButton = `<button>Procrastinate</button>`;
-        let doneButton = `<button>Done</button>
-         </div>`;
-        if (this.isDue)
-            return opening + item + dueButton + doneButton + closing;
-        else
-            return opening + item + doneButton + closing;
+
+    done() {
+        this.isDone = true;
+    }
+
+    render() {
+    
+            return `<div class="container">
+                        <div class="card-group row ${this.isDone ? `text-grey` : ``}">
+                        <div class="card col-sm-6">
+                            <div class="card-body">
+                            ${this.text}
+                            </div>
+                        </div>
+                        <div class="card col-sm-3 ${this.isDue ? `bg-danger text-white` : ``}">
+                            <div class="card-body text-right">
+                                <div>
+                                <span>${this.dueDate}</span>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="card col-sm-3">
+                                <div class="card-body text-right">
+                                <button class="btn btn-primary btn-sm" onclick="${this.done()}" >Done</button>
+                                ${this.isDue ? `<button id=${this.id} class="btn btn-secondary btn-sm">Procrastinate</button>` : ``}
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 // Crastinate App
 
-import "Item.js";
-
 const app = document.getElementById('root');
 const save = document.getElementById('save');
 const text = document.getElementById('todo_text');
-const dueDate = document.getElementById('duedate');
+const dueDate = document.getElementById('todo_duedate');
 
-function addTodo (todo) {
-    app.innerHTML += todo.template();
+
+function addTodo (item) {
+    app.innerHTML += item.render();
 };
 
+let todo = new Item (text.value, dueDate.value)
+
+// Press ENTER to display search results for input keyword
+text.addEventListener('keypress', function(e){
+        
+    if (e.keyCode == 13) {
+        addTodo(todo);
+    }
+
+});
+// Click "Search" to display search results for input keyword
+save.addEventListener('click', function() {
+    addTodo(todo);
+});
 
 
 
