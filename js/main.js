6645d45e63464435aca48eb3620b618f9e50487d
paulobef <beffpa@gmail.com>
2019-03-25 09:08:56 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 import "Item.js";
 
 const app = document.getElementById('root');
-
+const save = document.getElementById('save');
+const text = document.getElementById('todo_text');
+const dueDate = document.getElementById('duedate');
 
 function addTodo (todo) {
     app.innerHTML += todo.template();
