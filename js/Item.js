6909007703335604fbd7dfdf5f9625f241cc4843
Paul Beffa <paulbeffa@outlook.fr>
2019-03-26 19:17:52 +0100

    Reactive UI composant refactoring

--- js/Item.js
+++ js/Item.js
@@ -1,47 +1,80 @@
-const today = new Date();
 
-class Item {
-    constructor(text, dueDate, id) {
-        this.id = id
-        this.text = text;
-        this.dueDate = dueDate;
-        this.isDue = false;
-        this.isDone = false;
+document.componentRegistry = { };
+document.nextId = 0;
+document.today = new Date();
+
+
+class Component {
+  constructor() {
+    this._id = ++document.nextId;
+    document.componentRegistry[this._id] = this;
+  }
+}
+
+class Item extends Component {
+
+  constructor(props) {
+    super();
+
+    this.state = {
+      text: props.text,
+      dueDate: props.dueDate,
+      isDue: props.isDue,
+      isDone: props.isDone
     }
-    isDateDue() {
 
-        if (dueDate <= today) {
-            this.isDue = true;
-        }
-    }
-
-    done() {
-        this.isDone = true;
-    }
-
-    render() {
-    
-            return `<div class="container">
-                        <div class="card-group row ${this.isDone ? `text-grey` : ``}">
-                        <div class="card col-sm-6">
-                            <div class="card-body">
-                            ${this.text}
+    this.container = document.createElement('container');
+    this.container.setAttribute('id', this._id);
+    this.template = `
+                    <div class="card-group row ${this.state.isDone ? `text-grey` : ``}">
+                    <div class="card col-sm-6">
+                        <div class="card-body">
+                        ${this.state.text}
+                        </div>
+                    </div>
+                    <div class="card col-sm-3 ${this.state.isDue ? `bg-danger text-white` : ``}">
+                        <div class="card-body text-right">
+                            <div>
+                            <span>${this.state.dueDate}</span>
                             </div>
                         </div>
-                        <div class="card col-sm-3 ${this.isDue ? `bg-danger text-white` : ``}">
+                        </div>
+                        <div class="card col-sm-3">
                             <div class="card-body text-right">
-                                <div>
-                                <span>${this.dueDate}</span>
-                                </div>
-                            </div>
-                            </div>
-                            <div class="card col-sm-3">
-                                <div class="card-body text-right">
-                                <button class="btn btn-primary btn-sm" onclick="${this.done()}" >Done</button>
-                                ${this.isDue ? `<button id=${this.id} class="btn btn-secondary btn-sm">Procrastinate</button>` : ``}
-                                </div>
+                            <button class="btn btn-primary btn-sm" onclick="document.componentRegistry[${this._id}].isDone(this.value)" >Done</button>
+                            ${this.state.isDue ? `<button id=${this.id} class="btn btn-secondary btn-sm">Procrastinate</button>` : ``}
                             </div>
                         </div>
                     </div>`
+  }
+
+    isDue() {
+        this.state.isDue = true;
     }
+
+    isItDue() {
+        if (document.today > this.dueDate) {
+            this.isDue();
+        }
+
+    }
+
+    isDone() {
+        this.state.isDone = true;
+        this.update();
+    }
+
+    render() {
+
+            this.isItDue();
+
+            this.container.innerHTML = this.template;
+            app.innerHTML += this.container.innerHTML
+    }
+    update() {
+        document.getElementById(this._id).innerHTML = this.template;
+    }
+
+    
 }
+
--- js/main.js
+++ js/main.js
@@ -6,23 +6,32 @@ const text = document.getElementById('todo_text');
 const dueDate = document.getElementById('todo_duedate');
 
 
-function addTodo (item) {
-    app.innerHTML += item.render();
-};
 
-let todo = new Item (text.value, dueDate.value)
+
+let todoData = {
+    text: text.value,
+    dueDate: dueDate.value,
+    isDue: false,
+    isDone: false
+
+  };
+
+  let todo = new Item(todoData);
+
+
+
 
 // Press ENTER to display search results for input keyword
 text.addEventListener('keypress', function(e){
         
     if (e.keyCode == 13) {
-        addTodo(todo);
+       todo.render();
     }
 
 });
 // Click "Search" to display search results for input keyword
 save.addEventListener('click', function() {
-    addTodo(todo);
+    todo.render();
 });
 
 
